f3fc8fe1b6e12fef28bcbb00af75737250ee2047
Austin-Fulbright <austinfulbright@gmail.com>
2024-10-09 07:50:14 -0500

    Initial Commit

---
+++ VoiceRecorder/VoiceRecorderApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoiceRecorderApp.swift
+//  VoiceRecorder
+//
+//  Created by Austin Fulbright on 10/9/24.
+//
+
+import SwiftUI
+
+@main
+struct VoiceRecorderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
